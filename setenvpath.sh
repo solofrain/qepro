993fb76f0b99f6e499a9bbbf5f3269f94fa2719c
Wayne Lewis <wayne.lewis@esss.se>
2018-05-21 17:35:51 +0200

    Update environment script to use seabreeze

--- setenvpath.sh
+++ setenvpath.sh
@@ -3,12 +3,14 @@
 ##############################################################
 ###### if you want to use local
  #JAVA_JRE="/usr/java/jdk1.8.0_131/jre/lib/amd64/server"
- JAVA_JRE="/usr/java/jdk1.8.0_171-amd64/jre/lib/amd64/server"
+ #JAVA_JRE="/usr/java/jdk1.8.0_171-amd64/jre/lib/amd64/server"
  #OOI_HOME="/vagrant/modules/m-epics-qepro/omniDrv/OOI_HOME"
- OOI_HOME="/home/waynelewis/git/m-epics-qeproasyn/omniDrv/OOI_HOME"
+ #OOI_HOME="/home/waynelewis/git/m-epics-qeproasyn/omniDrv/OOI_HOME"
+ SEABREEZE=/home/waynelewis/git/seabreeze-3.0.11/SeaBreeze/lib
 
- export LD_LIBRARY_PATH=$JAVA_JRE:$OOI_HOME
- export OOI_HOME=$OOI_HOME
+ #export LD_LIBRARY_PATH=$JAVA_JRE:$OOI_HOME
+ export LD_LIBRARY_PATH=$SEABREEZE
+ #export OOI_HOME=$OOI_HOME
 
 ##############################################################
 ###### if you want to use from /opi/epics/modules.....
