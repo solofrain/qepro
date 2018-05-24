5b186aedef4842def011f3ae73d1be00f9202545
Wayne Lewis <wayne.lewis@esss.se>
2018-05-24 16:09:12 +0200

    Update formatting, add placeholders for Busy record

--- startup/qepro.cmd
+++ startup/qepro.cmd
@@ -1,5 +1,6 @@
 #require qeproAsyn,tomaszbrys
 require qeproasyn,waynelewis
+#require busy,1.6.0-ESS0
 
 #epicsEnvSet(EPICS_CA_ADDR_LIST,10.0.2.15)
  epicsEnvSet(EPICS_CA_MAX_ARRAY_BYTES,64000000)
@@ -11,8 +12,11 @@ epicsEnvSet("LASER",  "478")
 
 drvUSBQEProConfigure("$(PORT)","$(SIZE)","$(LASER)")
 
-asynSetTraceMask("$(PORT)", -1, 0x9)
+#asynSetTraceMask("$(PORT)", -1, 0x11)
+#asynSetTraceMask("$(PORT)", -1, 0x9)
+#asynSetTraceMask("$(PORT)", -1, 0xFF)
 #asynSetTraceMask("$(PORT)", -1, 0xF)
+asynSetTraceMask("$(PORT)", -1, 0x1)
 asynSetTraceIOMask("$(PORT)", -1, 0x2)
 
 dbLoadRecords(qepro.template, "PREFIX=$(PREFIX), PORT=$(PORT), ADDR=0, TIMEOUT=1, SIZE=$(SIZE), LASER=$(LASER)")
