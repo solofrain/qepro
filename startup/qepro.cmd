95a9eeae45d60bdab5ca451a1edb8dd818a27eff
Wayne Lewis <wayne.lewis@esss.se>
2018-05-24 16:18:23 +0200

    Add autosave infrastructure

--- startup/qepro.cmd
+++ startup/qepro.cmd
@@ -1,6 +1,7 @@
 #require qeproAsyn,tomaszbrys
 require qeproasyn,waynelewis
 #require busy,1.6.0-ESS0
+require autosave,5.7.0
 
 #epicsEnvSet(EPICS_CA_ADDR_LIST,10.0.2.15)
  epicsEnvSet(EPICS_CA_MAX_ARRAY_BYTES,64000000)
@@ -21,4 +22,24 @@ asynSetTraceIOMask("$(PORT)", -1, 0x2)
 
 dbLoadRecords(qepro.template, "PREFIX=$(PREFIX), PORT=$(PORT), ADDR=0, TIMEOUT=1, SIZE=$(SIZE), LASER=$(LASER)")
 
+## autosave/restore machinery
+save_restoreSet_Debug(0)
+save_restoreSet_IncompleteSetsOk(1)
+save_restoreSet_DatedBackupFiles(1)
+ 
+set_savefile_path("autosave","/save")
+set_requestfile_path("autosave","/req")
+ 
+#set_pass0_restoreFile("info_positions.sav")
+set_pass0_restoreFile("info_settings.sav")
+set_pass1_restoreFile("info_settings.sav")
+ 
+iocInit()
+ 
+## more autosave/restore machinery
+cd autosave/req
+makeAutosaveFiles()
+cd ../..
+create_monitor_set("info_positions.req", 5 , "")
+create_monitor_set("info_settings.req", 15 , "")
 
