1994123ca12c46423410b9c21ac8a5ab2ab29651
Wayne Lewis <wlewis@ospreydcs.com>
2018-06-04 14:57:42 +0200

    Set up for CCDB

--- startup/qepro.cmd
+++ startup/qepro.cmd
@@ -1,15 +1,30 @@
+#@field PREFIX
+#@type STRING
+
+#@field PORT
+#@type STRING
+
+#@field SIZE
+#@type INTEGER
+
+#@field LASER
+#@type INTEGER
+
+#@field EPICS_CA_MAX_ARRAY_BYTES
+#@type INTEGER
+
 require qeproasyn,0.0.4+
 require busy,1.6.0+
 require autosave,5.7.0
 require seabreeze,3.0.11-ESS0
 
 #epicsEnvSet(EPICS_CA_ADDR_LIST,10.0.2.15)
- epicsEnvSet(EPICS_CA_MAX_ARRAY_BYTES,64000000)
+#epicsEnvSet(EPICS_CA_MAX_ARRAY_BYTES,64000000)
 
-epicsEnvSet("PREFIX", "Sp1")
-epicsEnvSet("PORT",   "QEPro")
-epicsEnvSet("SIZE",   "3648")
-epicsEnvSet("LASER",  "478")
+#epicsEnvSet("PREFIX", "Sp1")
+#epicsEnvSet("PORT",   "QEPro")
+#epicsEnvSet("SIZE",   "3648")
+#epicsEnvSet("LASER",  "522")
 
 drvUSBQEProConfigure("$(PORT)","$(SIZE)","$(LASER)")
 
@@ -19,7 +34,8 @@ asynSetTraceMask("$(PORT)", -1, 0x0)
 #asynSetTraceMask("$(PORT)", -1, 0xF)
 #asynSetTraceMask("$(PORT)", -1, 0x11)
 #asynSetTraceMask("$(PORT)", -1, 0xFF)
-asynSetTraceIOMask("$(PORT)", -1, 0x2)
+asynSetTraceIOMask("$(PORT)", -1, 0x0)
+#asynSetTraceIOMask("$(PORT)", -1, 0x2)
 
 dbLoadRecords(qepro.template, "PREFIX=$(PREFIX), PORT=$(PORT), ADDR=0, TIMEOUT=1, SIZE=$(SIZE), LASER=$(LASER)")
 
